703de59b7b558c982fb0b1dfc50e8f9f976773f0
WNesham <William.Nesham@gmail.com>
2020-06-16 23:00:45 -0500

    Added an easter egg to Stairs and some exception handling.

--- Staircase/Program.cs
+++ Staircase/Program.cs
@@ -7,11 +7,30 @@ namespace Staircase
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a number of stairs to print.");
+            // Init input
+            int stairCount = 3;
             // Part 2
-            int stairCount = int.Parse(Console.ReadLine());
-            
+            try
+            {
+                stairCount = int.Parse(Console.ReadLine());
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+
+            // Check limits
+            if (stairCount < 0 || stairCount > 1999999999)
+            {
+                Console.WriteLine("Invalid number of stairs entered.\n" +
+                    "Please enter between [0 - 2000000000) many stairs.");
+                return;
+            }
+
+            // Stair loop
             for(int stair=stairCount; stair > 0; stair--)
             {
+                // Pixel loop
                 for(int pixel = 0; pixel <= stairCount; pixel++)
                 {
                     if (pixel < stair)
@@ -25,6 +44,12 @@ namespace Staircase
                 }
                 Console.WriteLine();
             }
+            // Easter egg
+            if (stairCount >= 50)
+            {
+                Console.WriteLine("Po: \"Ah. My old enemy: Stairs.\" -Kung Fu Panda" );
+            }
+            
         }
     }
 }
